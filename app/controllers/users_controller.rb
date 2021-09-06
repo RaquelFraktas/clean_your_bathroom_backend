44521aa1c17ba5b3045570450b407e4b58b65363
github username <github email address>
2021-09-06 00:32:42 -0400

    add a find or create by method for json data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
 
   def index
-      users= User.all
+    users= User.all
     render json: users
+  end
+
+  def create
+    user= User.find_or_create_by(user_params)
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
   
 end
