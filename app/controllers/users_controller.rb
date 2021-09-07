10c8b53ae3ed9770487d0b1cb3e794c0fc25a761
github username <github email address>
2021-09-06 21:30:35 -0400

    create sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  wrap_parameters :user, include: [:username, :password]
+  # wrap_parameters :user, include: [:username, :password]
 
   def index
     users= User.all
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
 
   def create
     user= User.find_or_create_by(user_params)
+    if user.save
+      session[:id] = user.id
+    end
     byebug
     #posting data wouldnt work without this method
     render json: user
