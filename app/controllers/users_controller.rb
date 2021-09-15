338188dce81c77a07f38bba73b4c48ff1a0c2231
github username <github email address>
2021-09-15 16:12:48 -0400

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,10 @@ class UsersController < ApplicationController
     render json: user
   end
 
-  # def update
-  #   user= User.find(params[:id])
-  #   score = Score.find_or_create_by(points: params[:_json])
-  #   # this needs to be unique by user- check the find or create under that user.id
-  #   if !user.scores.include?(score)
-  #     user.scores << score
-  #     #dont really need this line if we are using find_or_create_by
-  #   end
-  #   byebug
-  #   render json: user
-  # end
-
+  def show
+    user= User.find(params[:id])
+    render json: user
+  end
 
   private
 
